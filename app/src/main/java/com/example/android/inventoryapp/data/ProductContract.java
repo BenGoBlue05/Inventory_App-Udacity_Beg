a194ac9a7cd049857da580bc83e88e8f96d0bf79
BenGoBlue05 <bplewis5@gmail.com>
2016-07-28 12:00:59 -0500

    Add content provider

--- app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -1,5 +1,8 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,15 +10,30 @@ import android.provider.BaseColumns;
  */
 public final class ProductContract {
 
+    public static final String CONTENT_AUTHORITY = " com.example.android.inventoryapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PRODUCT = "product";
     public ProductContract() {}
 
     public static abstract class ProductEntry implements BaseColumns{
-        public static final String TABLE_NAME = "product_entry";
-        public static final String COLUMN_NAME = "product_entry";
-        public static final String COLUMN_SUPPLIER = "product_entry";
-        public static final String COLUMN_QUANTITY = "product_entry";
-        public static final String COLUMN_PRICE = "product_entry";
 
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_PRODUCT).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCT;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCT;
+
+        public static final String TABLE_NAME = "product";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_SUPPLIER = "supplier";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_PRICE = "price";
+
+        public static Uri buildProductUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -0,0 +1,130 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by bplewis5 on 7/28/16.
+ */
+public class ProductProvider extends ContentProvider{
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private ProductDbHelper mOpenHelper;
+    private static final int PRODUCT = 100;
+    private static final int PRODUCT_WITH_ID =  101;
+
+    static {
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY,
+                ProductContract.PATH_PRODUCT, PRODUCT);
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY,
+                ProductContract.PATH_PRODUCT + "/#", PRODUCT_WITH_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        Cursor retCursor;
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        switch (sUriMatcher.match(uri)){
+            case PRODUCT:
+                retCursor = db.query(
+                            ProductContract.ProductEntry.TABLE_NAME,
+                            projection,
+                            selection,
+                            selectionArgs,
+                            null,
+                            null,
+                            sortOrder
+                    );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (sUriMatcher.match(uri)){
+            case PRODUCT:
+                return ProductContract.ProductEntry.CONTENT_TYPE;
+            case PRODUCT_WITH_ID:
+                return ProductContract.ProductEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues contentValues) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        Uri returnUri;
+
+        switch (sUriMatcher.match(uri)){
+            case PRODUCT:
+                long _id = db.insert(ProductContract.ProductEntry.TABLE_NAME, null, contentValues);
+                if (_id > 0)
+                    returnUri = ProductContract.ProductEntry.buildProductUri(_id);
+                else
+                    throw new SQLException("Failed to insert row into " + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int rowsDeleted;
+
+        switch (sUriMatcher.match(uri)){
+            case PRODUCT:
+                rowsDeleted = db.delete(ProductContract.ProductEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsDeleted != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int rowsUpdated;
+
+        switch (sUriMatcher.match(uri)){
+            case PRODUCT:
+                rowsUpdated = db.update(
+                        ProductContract.ProductEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+}
